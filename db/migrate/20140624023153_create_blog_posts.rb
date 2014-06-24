2a64034c729190dcf7370166dd46048757b0cd6b
Rachel Grau <rgrau@stanford.edu>
2014-06-24 09:57:20 -0700

    Added some models for blog posts and blog comments, and made link on homepage to blog work.

---
+++ db/migrate/20140624023153_create_blog_posts.rb
@@ -0,0 +1,7 @@
+class CreateBlogPosts < ActiveRecord::Migration
+  def change
+    create_table :blog_posts do |t|
+    	t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20140624023222_create_blog_comments.rb
@@ -0,0 +1,8 @@
+class CreateBlogComments < ActiveRecord::Migration
+  def change
+    create_table :blog_comments do |t|
+    	t.integer :blog_post_id
+    	t.timestamps
+    end
+  end
+end
