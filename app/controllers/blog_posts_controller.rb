fe14b7d3d331ed913b292efeb752de37294a69b5
Rachel Grau <rgrau@stanford.edu>
2014-12-01 21:44:44 +0100

    clean up a little

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,9 +1,6 @@
 class BlogPostsController < ApplicationController
 	#display all blog posts
 	def index
-	#	if (params[:id] != nil) then
-	#		flash.notice = "hi"
-	#	end
 		if (params[:delete]) then
 			blog_post = BlogPost.find_by_id(params[:id])
 			blog_post.destroy
@@ -14,14 +11,15 @@ class BlogPostsController < ApplicationController
 	def show
 	end
 
+	#start a new blog post
 	#only enabled if user is an admin
 	def new
 		@blog_post = BlogPost.new
 	end
 
+	#store a new blog post in DB and then display index page w/ all blog posts
 	#only enabled if user is an admin
 	def create
-		@message = "nice!"
 		bp = BlogPost.new
 		bp.title = params[:blog_post_title]
 		bp.content = params[:blog_post_content]
@@ -30,11 +28,10 @@ class BlogPostsController < ApplicationController
 		redirect_to :action => :index 
 	end
 
+	#delete a blog post
 	def destroy
 		blog_post = BlogPost.find_by_id(params[:id])
 		blog_post.destroy
 		redirect_to :action => :index
-
-		@hey = "hi"
 	end
 end
